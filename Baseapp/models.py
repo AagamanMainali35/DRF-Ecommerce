ed7c73daea1ad464c5b5c929ccaa57e92c98d183
aagaman mainali <aagamanmainali35@gmail.com>
2025-07-22 15:12:21 +0545

    Updated Model.py

--- Baseapp/models.py
+++ Baseapp/models.py
@@ -6,15 +6,14 @@ class Profile(models.Model):
         ('Female', 'Female'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_id = models.CharField(max_length=50, unique=True, default='#000')
+    profile_id = models.CharField(max_length=50, unique=True, default='#000') 
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True, null=True)
     role = models.CharField(max_length=20, default='customer')
     def __str__(self):
         return f'Profile of  "{self.user.username}" '
-   
-    
+       
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
