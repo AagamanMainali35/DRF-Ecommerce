ca0e59e2293c005aa87b19b05e92775c79869a54
aagaman mainali <aagamanmainali35@gmail.com>
2026-02-25 10:19:47 +0545

    renmed the pacakged file and added to url allowed host

--- DRF_Ecommerce/settings.py
+++ DRF_Ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-##ax1)%h!noylf1nkz7dwx$t^t@!gn)@d16^ogm=8@c6agm^4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://drf-ecommerce-irjl.onrender.com']
 
 
 # Application definition
